702437db3db2fee91f2f1a87aa66afae5c61a717
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-09-12 14:19:58 -0300

    card route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import annotationRoute from "./annotationRoute";
 import authRoute from "./authRoute";
+import cardRoute from "./cardRoute";
 import credentialRoute from "./credentialsRoute";
 
 const router = Router();
 
 router.use(authRoute);
 router.use(credentialRoute);
-router.use(annotationRoute)
+router.use(annotationRoute);
+router.use(cardRoute);
 
 export default router;
